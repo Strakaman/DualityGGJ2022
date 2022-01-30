cbc70ba3cede82f25319b0c4b0ade5486696d086
Strakaman <straka3000@gmail.com>
2022-01-29 21:13:20 -0800

    Switch from hit scan to projectile.

---
+++ Assets/Scripts/CamTrackObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamTrackObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //Only used for testing bullet, basic script that tracks and object without having to be it's child
+
+    public GameObject objectToTrack;
+
+    private void Update()
+    {
+        if (objectToTrack)
+        {
+            Vector3 pos = objectToTrack.transform.localPosition;
+            transform.localPosition = new Vector3(pos.x, pos.y, pos.z - 10);
+        }
+    }
+}
